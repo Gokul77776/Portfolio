561f6ae4c435de0134fe66797ad3c948f6f1052e
Gokul77776 <gokulgo9845204595@gmail.com>
2025-07-12 21:18:22 +0530

    sixth commit

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,4 +1,4 @@
-import { Briefcase, FileText, Home, User } from "lucide-react";
+import { ChevronsLeftRightEllipsis , CircleUserRound , Home, User } from "lucide-react";
 import { NavBar } from "./components/ui/tubelight-navbar";
 import { Footer } from "./components/ui/footer";
 
@@ -9,9 +9,9 @@ type LayoutProps ={
 export const Layout: React.FC<LayoutProps> =  ({children })=>{
       const navItems = [
     { name: 'Home', url: '/', icon: Home },
-    { name: 'About', url: '/about', icon: User },
-    { name: 'Skills', url: '/skills', icon: Briefcase },
-    { name: 'Contact Me', url: '/contact', icon: FileText },
+    { name: 'About', url: '/about', icon:CircleUserRound   },
+    { name: 'Skills', url: '/skills', icon: ChevronsLeftRightEllipsis  },
+    { name: 'Contact Me', url: '/contact', icon: User },
      
 
   ]
